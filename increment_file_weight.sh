48ed0d982f6d5518d6e6ca22d04ad54a3f4a8b95
lassejoe <lj@clearhaus.com>
2022-09-26 09:57:29 +0200

    Update bash scripts

--- increment_file_weight.sh
+++ increment_file_weight.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 new_weight=0
-echo "Please select the desired job:"
+echo "All files weight greater than the selected files weight will be incremented"
+echo "Select file:"
 select choice in $(find ./website/content -name "*.md" -type f); do
     [[ -n $choice ]] || { echo "Invalid choice. Please try again." >&2; continue; }
     selected_weight=$(cat $choice | grep "weight"| grep -o '[[:digit:]]*') 
     for f in $(find ./website/content -name "*.md" -type f); do
         current_weight=$(cat $f | grep "weight"| grep -o '[[:digit:]]*') 
         if [ "$current_weight" -gt "$selected_weight" ]; then
-            echo $current_weight
             ((new_weight=current_weight+5))
-            echo $new_weight
             sed -i "s/weight: $current_weight/weight: $new_weight/" $f
         fi
     done
