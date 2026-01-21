caa6cd113b4a40c30904f0beb54446c82d9ce522
Peter Klogborg <pk@clearhaus.com>
2026-01-21 13:23:44 +0100

    Update start.sh to mount current directory in Docker container for development workflow.

--- build/start.sh
+++ build/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -it --name gateway-api-docs -p 1313:1313 clearhaus/gateway-api-docs sh -c "yarn install && yarn serve"
+docker run --rm -it --name gateway-api-docs -p 1313:1313 -v $PWD:/opt/clearhaus/gateway-api-docs clearhaus/gateway-api-docs sh -c "yarn install && yarn serve"
